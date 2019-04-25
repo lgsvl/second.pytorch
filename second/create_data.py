ebef56c018f545ccb57405d237a4fe2e9ee6f83a
David Uhm <david.uhm@lge.com>
2019-04-24 18:18:46 -0700

    do not use testing dataset for faster processing

--- second/create_data.py
+++ second/create_data.py
@@ -57,9 +57,9 @@ def create_kitti_info_file(data_path,
                            create_trainval=False,
                            relative_path=True):
     t0 = time.time()
-    train_img_ids = _read_imageset_file("/root/data/kitti/train.txt")
-    val_img_ids = _read_imageset_file("/root/data/kitti/val.txt")
-    test_img_ids = _read_imageset_file("/root/data/kitti/test.txt")
+    train_img_ids = _read_imageset_file("/root/data/kitti/training/train.txt")
+    val_img_ids = _read_imageset_file("/root/data/kitti/testing/val.txt")
+    # test_img_ids = _read_imageset_file("/root/data/kitti/test.txt")
 
     print("Generate info. this may take several minutes.")
     if save_path is None:
@@ -80,7 +80,7 @@ def create_kitti_info_file(data_path,
         pickle.dump(kitti_infos_train, f)
     kitti_infos_val = kitti.get_kitti_image_info(
         data_path,
-        training=True,
+        training=False,
         velodyne=True,
         calib=True,
         image_ids=val_img_ids,
@@ -104,23 +104,23 @@ def create_kitti_info_file(data_path,
         with open(filename, 'wb') as f:
             pickle.dump(kitti_infos_trainval, f)
     """
-    filename = save_path / 'kitti_infos_trainval.pkl'
-    print(f"Kitti info trainval file is saved to {filename}")
-    with open(filename, 'wb') as f:
-        pickle.dump(kitti_infos_train + kitti_infos_val, f)
+    # filename = save_path / 'kitti_infos_trainval.pkl'
+    # print(f"Kitti info trainval file is saved to {filename}")
+    # with open(filename, 'wb') as f:
+    #     pickle.dump(kitti_infos_train + kitti_infos_val, f)
 
-    kitti_infos_test = kitti.get_kitti_image_info(
-        data_path,
-        training=False,
-        label_info=False,
-        velodyne=True,
-        calib=True,
-        image_ids=test_img_ids,
-        relative_path=relative_path)
-    filename = save_path / 'kitti_infos_test.pkl'
-    print(f"Kitti info test file is saved to {filename}")
-    with open(filename, 'wb') as f:
-        pickle.dump(kitti_infos_test, f)
+    # kitti_infos_test = kitti.get_kitti_image_info(
+    #     data_path,
+    #     training=False,
+    #     label_info=False,
+    #     velodyne=True,
+    #     calib=True,
+    #     image_ids=test_img_ids,
+    #     relative_path=relative_path)
+    # filename = save_path / 'kitti_infos_test.pkl'
+    # print(f"Kitti info test file is saved to {filename}")
+    # with open(filename, 'wb') as f:
+    #     pickle.dump(kitti_infos_test, f)
 
     print("Elapsed time: {:.3f} s".format(time.time() - t0))
 
@@ -172,19 +172,19 @@ def create_reduced_point_cloud(data_path,
         train_info_path = pathlib.Path(data_path) / 'kitti_infos_train.pkl'
     if val_info_path is None:
         val_info_path = pathlib.Path(data_path) / 'kitti_infos_val.pkl'
-    if test_info_path is None:
-        test_info_path = pathlib.Path(data_path) / 'kitti_infos_test.pkl'
+    # if test_info_path is None:
+    #     test_info_path = pathlib.Path(data_path) / 'kitti_infos_test.pkl'
 
     _create_reduced_point_cloud(data_path, train_info_path, save_path)
     _create_reduced_point_cloud(data_path, val_info_path, save_path)
-    _create_reduced_point_cloud(data_path, test_info_path, save_path)
+    # _create_reduced_point_cloud(data_path, test_info_path, save_path)
     if with_back:
         _create_reduced_point_cloud(
             data_path, train_info_path, save_path, back=True)
         _create_reduced_point_cloud(
             data_path, val_info_path, save_path, back=True)
-        _create_reduced_point_cloud(
-            data_path, test_info_path, save_path, back=True)
+        # _create_reduced_point_cloud(
+        #     data_path, test_info_path, save_path, back=True)
 
     print("Elapsed time: {:.3f} s".format(time.time() - t0))
 
